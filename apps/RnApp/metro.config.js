9f4fb0a17c7fbb9a151356fe43027c2a1cc964bd
Ki-Lok, Shin <newrecord@naver.com>
2026-02-06 21:10:49 +0900

    fix: configure Metro watchFolders for monorepo root node_modules resolution

--- apps/RnApp/metro.config.js
+++ apps/RnApp/metro.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const monorepoRoot = path.resolve(__dirname, '../..');
 
 /**
  * Metro configuration
@@ -6,6 +9,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [monorepoRoot],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(monorepoRoot, 'node_modules'),
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
